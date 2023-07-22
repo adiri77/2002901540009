8bca182465f859acbe4db77716397b8c6cc9d8e8
Aditya Kumar Singh <adityasingh321100@gmail.com>
2023-07-22 14:08:30 +0530

    First Commit

---
+++ app/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [urls, setUrls] = useState('');
+  const [result, setResult] = useState(null);
+
+  const handleGetNumbers = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8008/numbers?url=${encodeURIComponent(urls)}`);
+      setResult(response.data);
+    } catch (error) {
+      setResult(null);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Number Management Service</h1>
+      <p>Enter URLs separated by commas:</p>
+      <input
+        type="text"
+        value={urls}
+        onChange={(e) => setUrls(e.target.value)}
+        placeholder="http://20.244.56.144/numbers/primes,http://abc.com/fibo,..."
+      />
+      <button onClick={handleGetNumbers}>Get Numbers</button>
+      {result && (
+        <div>
+          <h2>Result:</h2>
+          <p>{JSON.stringify(result)}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
